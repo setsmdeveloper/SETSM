5fcf2b1d25e5c2f713c4183b653556a2d4a8500e
noh.56 <noh.56@osu.edu>
2017-10-16 12:05:19 -0400

    test1

--- setsm_code.c
+++ setsm_code.c
@@ -1383,10 +1383,10 @@ void SETSMmainfunction(TransParam *return_param, char* _filename, ARGINFO args, 
                                 int pts_row = (int)(floor(index/Tcols));
                                 int pts_col = index % Tcols;
                                 printf("row col %d\t%d\n",pts_row,pts_col);
-                                fprintf(pFile_info1,"%d\t%d\t%d\t%f\n",pts_row+1,pts_col+1,tiles[index],Theight[index].m_Y-Theight[index].m_X);
+                                fprintf(pFile_info1,"%d\t%d\t%d\t%lf\n",pts_row+1,pts_col+1,tiles[index],Theight[index].m_Y-Theight[index].m_X);
                             }
                             printf("end writing tiles info\n");
-                            fclose(pFile_info1);
+                            //fclose(pFile_info1);
                         }
                         
                         printf("end fclose1\n");
