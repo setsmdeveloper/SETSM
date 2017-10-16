700b1373c8f63d3386487df1dd50513029854410
noh.56 <noh.56@osu.edu>
2017-10-16 11:45:40 -0400

    test1

--- setsm_code.c
+++ setsm_code.c
@@ -1353,6 +1353,7 @@ void SETSMmainfunction(TransParam *return_param, char* _filename, ARGINFO args, 
                         i = 0;
                         while( /*i < final_count_MPs && */(fscanf(pFile,"%lf %lf %lf\n",&ptslists.m_X,&ptslists.m_Y,&ptslists.m_Z)) != EOF )
                         {
+                            printf("XYZ %f\t%f\t%f\n",ptslists.m_X,ptslists.m_Y,ptslists.m_Z);
                             int temp_col = floor((ptslists.m_X - Boundary[0])/subX);
                             int temp_row = floor((ptslists.m_Y - Boundary[1])/subY);
                             
