182854ad40d9c0d5408f70cfa05d42dee6b30c5e
Caleb Lehman <lehman.346@osu.edu>
2018-06-26 13:21:31 -0400

    Fixed geotiff bugs

--- setsmgeo.c
+++ setsmgeo.c
@@ -11,64 +11,66 @@ static const TIFFFieldInfo xtiffFieldInfo[] = {
     { GDAL_NODATA, -1, -1, TIFF_ASCII, FIELD_CUSTOM, 1, 0, "GDAL_NODATA" }
 };
 
+#define STRIP_SIZE_DEFAULT 8192
+#define max(a, b)  (((a) > (b)) ? (a) : (b))
+
 int WriteGeotiff_DEM(char *filename, void *buffer, size_t width, size_t height, double scale, double minX, double maxY, int projection, int zone, int NS_hemisphere, int data_type)
 {
     TIFF *tif;  /* TIFF-level descriptor */
     GTIF *gtif; /* GeoKey-level descriptor */
-    
-    tif=XTIFFOpen(filename,"w");
+    size_t bytes = 0; /* Size of data type for computing buffer offset */
+
+    switch (data_type)
+    {
+        case FLOAT:
+            bytes = sizeof(float);
+            break;
+        case UCHAR:
+            bytes = sizeof(unsigned char);
+            break;
+        case UINT16:
+            bytes = sizeof(uint16);
+            break;
+        default:
+            printf("unrecognized data type: %d\n", data_type);
+            break;
+    }
+
+    if (bytes == 0)
+    { 
+        // Unknown data type
+        return -1;
+    }
+
+    tif = XTIFFOpen(filename, "w");
     if (!tif)
     {
-        printf("failed in XTIFFOpen\n");
-        goto failure;
+        printf("WriteGeotiff failed in XTIFFOpen\n");
+        return -1;
     }
     
     gtif = GTIFNew(tif);
     if (!gtif)
     {
-        printf("failed in GTIFNew\n");
-        goto failure;
+        printf("WriteGeotiff failed in GTIFNew\n");
+        XTIFFClose(tif);
+        return -1;
     }
     
     SetUpTIFFDirectory(tif, width, height, scale, minX, maxY, data_type);
     SetUpGeoKeys(gtif, projection, zone, NS_hemisphere);
-
     for (int row=0; row<height; row++)
     {
-    // Write from buffer after appropriate cast and collect status of write
-        int status;
-        switch (data_type)
+        if (TIFFWriteScanline(tif, ((char *)buffer) + (bytes * row * width), row, 0) == -1) // TODO: TIFFWriteScanline may return -1 on failure:
         {
-        case FLOAT:
-            status = TIFFWriteScanline(tif, ((float *)buffer) + (row * width), row, 0);
-        break;
-        case UCHAR:
-            status = TIFFWriteScanline(tif, ((unsigned char *)buffer) + (row * width), row, 0);
-        break;
-        case UINT16:
-            status = TIFFWriteScanline(tif, ((uint16 *)buffer) + (row * width), row, 0);
-        break;
-        default:
-            printf("unrecognized data type: %d\n", data_type);
-            goto failure;
-        break;
-        }
-
-        if (!status) // TODO: TIFFWriteScanline may return -1 on failure:
-        {
-            TIFFError("WriteGeotiff_DEM","failure in WriteScanline\n");
+            TIFFError("WriteGeotiff_DEM","failure in WriteScanline on row %d\n", row);
         }
     }
-    
+                
     GTIFWriteKeys(gtif);
-    GTIFFree(gtif);
+    GTIFFree(gtif); 
     XTIFFClose(tif);
     return 0;
-    
-failure:
-    if (tif) TIFFClose(tif);
-    if (gtif) GTIFFree(gtif);
-    return -1;
 }
 
 
@@ -86,30 +88,34 @@ int SetUpTIFFDirectory(TIFF *tif, size_t width, size_t height, double scale, dou
     TIFFSetField(tif, TIFFTAG_COMPRESSION, COMPRESSION_LZW);
     TIFFSetField(tif, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);
     TIFFSetField(tif, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
-    TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, 1);
     TIFFSetField(tif, TIFFTAG_GEOTIEPOINTS, 6,tiepoints);
     TIFFSetField(tif, TIFFTAG_GEOPIXELSCALE, 3,pixscale);
-
-    TIFFMergeFieldInfo(tif, xtiffFieldInfo, N(xtiffFieldInfo));
-    TIFFSetField(tif, GDAL_NODATA, "-9999");
-
+    TIFFSetField(tif, TIFFTAG_PREDICTOR, 1);
+    TIFFSetField(tif, TIFFTAG_SAMPLESPERPIXEL, 1);
+    
     switch (data_type)
     {
-    case FLOAT:
-        TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 32);
-        TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_IEEEFP);
-    break;
-    case UCHAR:
-        TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 8);
-        TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_UINT);
-    break;
-    case UINT16:
-        TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 16);
-        TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_UINT);
-    break;
-    default:
-        printf("unrecognized data type:  %d\n", data_type);
-    break;
+        case FLOAT:
+            TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 32);
+            TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_IEEEFP);
+            TIFFMergeFieldInfo(tif, xtiffFieldInfo, N(xtiffFieldInfo));
+            TIFFSetField(tif, GDAL_NODATA, "-9999");
+            TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, max(1, (STRIP_SIZE_DEFAULT * 8) / (width * 32)));
+            break;
+        case UCHAR:
+            TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 8);
+            TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_UINT);
+            TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, 2);
+            TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, max(1, (STRIP_SIZE_DEFAULT * 8) / (width * 8)));
+            break;
+        case UINT16:
+            TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 16);
+            TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_UINT);
+            TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, 1);
+            TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, max(1, (STRIP_SIZE_DEFAULT * 8) / (width * 16)));
+            break;
+        default:
+            break;
     }
 }
 
@@ -130,27 +136,36 @@ int SetUpGeoKeys(GTIF *gtif, int projection, int zone, int NS_hemisphere)
         GTIFKeySet(gtif, ProjectedCSTypeGeoKey, TYPE_SHORT, 1, 32767);  // user-defined
         GTIFKeySet(gtif, ProjectionGeoKey, TYPE_SHORT, 1, 32767);  // user-defined
         GTIFKeySet(gtif, ProjCoordTransGeoKey, TYPE_SHORT, 1, CT_PolarStereographic);
-        GTIFKeySet(gtif, ProjNatOriginLatGeoKey, TYPE_DOUBLE, 1, 70.0);
         GTIFKeySet(gtif, ProjFalseEastingGeoKey, TYPE_DOUBLE, 1, 0.0);
         GTIFKeySet(gtif, ProjFalseNorthingGeoKey, TYPE_DOUBLE, 1, 0.0);
         GTIFKeySet(gtif, ProjScaleAtNatOriginGeoKey, TYPE_DOUBLE, 1, 1.0);
-        GTIFKeySet(gtif, ProjStraightVertPoleLongGeoKey, TYPE_DOUBLE, 1, -45.0);
+
+        if (NS_hemisphere != 0)
+        {
+            GTIFKeySet(gtif, ProjNatOriginLatGeoKey, TYPE_DOUBLE, 1, 70.0);
+            GTIFKeySet(gtif, ProjStraightVertPoleLongGeoKey, TYPE_DOUBLE, 1, -45.0);
+        }
+        else
+        {
+            GTIFKeySet(gtif, ProjNatOriginLatGeoKey, TYPE_DOUBLE, 1, -71.0);
+            GTIFKeySet(gtif, ProjStraightVertPoleLongGeoKey, TYPE_DOUBLE, 1, 0.0);
+        }
     }
     else  // UTM
     {
-    char GeoKeyStr[100];
-    if (NS_hemisphere != 0)
-    {
-        sprintf(GeoKeyStr, "UTM Zone %d, Northern Hemisphere", zone);
-        GTIFKeySet(gtif, GTCitationGeoKey, TYPE_ASCII, 0, GeoKeyStr);
-        GTIFKeySet(gtif, ProjectedCSTypeGeoKey, TYPE_SHORT, 1, PCS_WGS84_UTM_zone_1N - 1 + zone);
-    }
-    else
-    {
-        sprintf(GeoKeyStr, "UTM Zone %d, Southern Hemisphere", zone);
-        GTIFKeySet(gtif, GTCitationGeoKey, TYPE_ASCII, 0, GeoKeyStr);
-        GTIFKeySet(gtif, ProjectedCSTypeGeoKey, TYPE_SHORT, 1, PCS_WGS84_UTM_zone_1S - 1 + zone);
-    }
+        char GeoKeyStr[500];
+        if (NS_hemisphere != 0)
+        {
+            sprintf(GeoKeyStr, "UTM Zone %d, Northern Hemisphere", zone);
+            GTIFKeySet(gtif, GTCitationGeoKey, TYPE_ASCII, 0, GeoKeyStr);
+            GTIFKeySet(gtif, ProjectedCSTypeGeoKey, TYPE_SHORT, 1, PCS_WGS84_UTM_zone_1N - 1 + zone);
+        }
+        else
+        {
+            sprintf(GeoKeyStr, "UTM Zone %d, Southern Hemisphere", zone);
+            GTIFKeySet(gtif, GTCitationGeoKey, TYPE_ASCII, 0, GeoKeyStr);
+            GTIFKeySet(gtif, ProjectedCSTypeGeoKey, TYPE_SHORT, 1, PCS_WGS84_UTM_zone_1S - 1 + zone);
+        }
     }
 }
 
