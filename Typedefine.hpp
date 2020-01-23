3a6b5d34c06a64cff9ab46981ba7000c701b03c1
noh.56 <noh.56@osu.edu>
2020-01-23 14:23:32 -0500

    multi varialble deleted

--- Typedefine.hpp
+++ Typedefine.hpp
@@ -119,7 +119,7 @@ typedef struct tagNCCresult
 	float result2; //first peak height
 	float result3; //second peak height
     float GNCC;
-    float *GNCC_multi;
+    //float *GNCC_multi;
 	int result4; //peak count
     double max_WNCC;
     int max_WNCC_pos;
@@ -151,7 +151,7 @@ typedef struct tagVoxelinfo
     float height;
     bool flag_cal;
     float INCC;
-    float *INCC_multi;
+    //float *INCC_multi;
 }VOXEL;
 
 typedef struct LSFinfo{
