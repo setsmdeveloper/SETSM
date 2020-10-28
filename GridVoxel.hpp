703497209a39137220be1ec23bcf6469369d4d89
Evan Danish <edanish@osc.edu>
2020-10-28 09:16:55 -0400

    Remove extra comments

--- GridVoxel.hpp
+++ GridVoxel.hpp
@@ -13,49 +13,6 @@ inline double SignedCharToDouble_voxel(short val)
     return (double)(val)/1000.0;
 }
 
-
-
-/*
- * Allocated like: 
-        grid_voxel = (VOXEL**)calloc(sizeof(VOXEL*),Size_Grid2D.width*Size_Grid2D.height);
- *
- * That is, each point in the voxel grid is a VOXEL * (VOXEL pointer)
- * Passed to InitializeVoxel. First thing this function does is free older
- * version of the voxel arrays:
- *
-        free(grid_voxel[t_i]);
-  
- * Then, allocates them:
-        grid_voxel[t_i] = (VOXEL*)calloc(sizeof(VOXEL),NumberofHeightVoxel);
- *
- * And, for each height index, updates the INCC and flag_cal:
-        for(int h = 0 ; h < NumberofHeightVoxel ; h++)
-        {
-            for(int pair = 0 ; pair < plevelinfo.pairinfo->NumberOfPairs; pair++)
-            {
-                grid_voxel[t_i][h].flag_cal[pair] = false;
-                grid_voxel[t_i][h].INCC[pair] = DoubleToSignedChar_voxel(-1);
-            }
-        }
- *
- *
- * Used in AWNCC_[AWNCC, SGM, single, multi), InitializeVoxel,
- * SGM_[con, start]_pos, VerticalLineLocus, SignedCharToDouble_voxel
- */
-
-/*
-typedef struct tagVoxelinfo
-{
-    //float ANCC;
-    //float height;
-    bool flag_cal[MaxNCC];
-    short INCC[MaxNCC];
-    //float *INCC_multi;
-}VOXEL;
-*/
-
-
-
 class VoxelTower {
 public:
     VoxelTower(int max_ncc) : ncc_len(max_ncc) {}
@@ -105,6 +62,7 @@ private:
 
 };
 
+// Thin wrapper around vector, passes through the max_ncc value
 class GridVoxel {
 public:
     typedef std::vector<VoxelTower>::size_type size_type;
@@ -127,4 +85,3 @@ private:
 };
 
 #endif
-
