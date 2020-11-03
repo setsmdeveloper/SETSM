b6e001270c1f5d0afad1d7db9441e6f65c62f700
Samuel Khuvis <skhuvis@osc.edu>
2020-11-03 11:51:04 -0500

    Fix bug in single_printf function

--- log.cpp
+++ log.cpp
@@ -114,7 +114,7 @@ int single_printf(const char *fmt, ...) {
 #endif
     va_list ap;
     va_start(ap, fmt);
-    int ret = printf(fmt, ap);
+    int ret = vprintf(fmt, ap);
     va_end(ap);
     return ret;
 }
