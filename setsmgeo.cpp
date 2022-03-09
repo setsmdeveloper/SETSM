7262bd0abe09e659110277875234329bb6ed16df
Jim Klassen <klassen.js@gmail.com>
2022-03-08 18:09:51 -0600

    Fix WriteGeotiff with PREDICTOR=3.
    TIFFWriteScanline modifies its source buffer when the PREDICTOR != 1.
    
    Use a second scanline buffer so any changes from rounding or from
    TIFFWriteScanline don't change the source buffer.
    
    Also enables PREDICTOR=3 for all FLOAT output since this is now safe.
    
    Enabling PREDICTOR=3 is lossless and reduces file sizes, although
    not as much as also rounding to 1/128.
    
    One example (8m, 1938x1709, LZW Compression):
    
      |     | PREDICTOR=1 | PREDICTOR=3 | PREDICTOR=3 & rounded 1/128
      |=====+=============+=============+============================
      | DEM |    13 MiB   |   9.2 MiB   |   5.2 MiB
      | LSF |    13 MiB   |   9.0 MiB   |   4.8 MiB
    
    Note: LSF still requires a non-rounded DEM as input to avoid artifacts.

--- setsmgeo.cpp
+++ setsmgeo.cpp
@@ -18,7 +18,8 @@ static const TIFFFieldInfo xtiffFieldInfo[] = {
 #define STRIP_SIZE_DEFAULT 8192
 #define max(a, b)  (((a) > (b)) ? (a) : (b))
 
-int WriteGeotiff(char *filename, void *buffer, size_t width, size_t height, double scale, double minX, double maxY, int projection, int zone, int NS_hemisphere, int data_type)
+
+int WriteGeotiff_real(char *filename, void *buffer, size_t width, size_t height, double scale, double minX, double maxY, int projection, int zone, int NS_hemisphere, int data_type, bool doRound)
 {
     TIFF *tif;  /* TIFF-level descriptor */
     GTIF *gtif; /* GeoKey-level descriptor */
@@ -67,14 +68,34 @@ int WriteGeotiff(char *filename, void *buffer, size_t width, size_t height, doub
 
     SetUpTIFFDirectory(tif, width, height, scale, minX, maxY, data_type);
     SetUpGeoKeys(gtif, projection, zone, NS_hemisphere);
-    
+
+    // scanline_buffer is needed because TIFFWriteScanline modifies the buffer if PREDICTOR != 1.
+    void *scanline_buffer = malloc(bytes * width);
+    if (!scanline_buffer) {
+        printf("WriteGeotiff failed in malloc scanline_buffer\n");
+        return -1;
+    }
+
     for (int row=0; row<height; row++)
     {
-        if (TIFFWriteScanline(tif, ((char *)buffer) + (bytes * row * width), row, 0) == -1) // TODO: TIFFWriteScanline may return -1 on failure:
+        memcpy(scanline_buffer, ((char *)buffer) + (bytes * row * width), bytes * width);
+
+        // If requested, round values to nearest 1/128
+        if (doRound && data_type == FLOAT) {
+            for(size_t i = 0 ; i < width; i++)
+            {
+                float &val = ((float*)scanline_buffer)[i];
+                val = std::round(val*128) / 128;
+            }
+        }
+
+        if (TIFFWriteScanline(tif, scanline_buffer, row, 0) == -1) // TODO: TIFFWriteScanline may return -1 on failure:
         {
             TIFFError("WriteGeotiff_DEM","failure in WriteScanline on row %d\n", row);
         }
     }
+
+    free(scanline_buffer);
     
     GTIFWriteKeys(gtif);
     GTIFFree(gtif);
@@ -82,73 +103,15 @@ int WriteGeotiff(char *filename, void *buffer, size_t width, size_t height, doub
     return 0;
 }
 
+
+int WriteGeotiff(char *filename, void *buffer, size_t width, size_t height, double scale, double minX, double maxY, int projection, int zone, int NS_hemisphere, int data_type)
+{
+	return WriteGeotiff_real(filename, buffer, width, height, scale, minX, maxY, projection, zone, NS_hemisphere, data_type, false);
+}
+
 int WriteGeotiff_round(char *filename, void *buffer, size_t width, size_t height, double scale, double minX, double maxY, int projection, int zone, int NS_hemisphere, int data_type)
 {
-    TIFF *tif;  /* TIFF-level descriptor */
-    GTIF *gtif; /* GeoKey-level descriptor */
-    size_t bytes = 0; /* Size of data type for computing buffer offset */
-
-    switch (data_type)
-    {
-        case FLOAT:
-            bytes = sizeof(float);
-            for(size_t i = 0 ; i < width * height; i++)
-            {
-                float &val = ((float*)buffer)[i];
-                val = std::round(val*128) / 128;
-            }
-            break;
-        case UCHAR:
-            bytes = sizeof(unsigned char);
-            break;
-        case UINT16:
-            bytes = sizeof(uint16);
-            break;
-        default:
-            printf("unrecognized data type: %d\n", data_type);
-            break;
-    }
-
-    if (bytes == 0)
-    {
-        // Unknown data type
-        return -1;
-    }
-
-    long int int_mem = (long)(bytes*height*width);
-    double tif_mem = (double)(int_mem/1024.0/1024.0/1024.0);
-    printf("tif mem %f\n",tif_mem);
-
-    tif = XTIFFOpen(filename, "w8");
-    if (!tif)
-    {
-        printf("WriteGeotiff failed in XTIFFOpen\n");
-        return -1;
-    }
-
-    gtif = GTIFNew(tif);
-    if (!gtif)
-    {
-        printf("WriteGeotiff failed in GTIFNew\n");
-        XTIFFClose(tif);
-        return -1;
-    }
-
-    SetUpTIFFDirectory_round(tif, width, height, scale, minX, maxY, data_type);
-    SetUpGeoKeys(gtif, projection, zone, NS_hemisphere);
-    
-    for (int row=0; row<height; row++)
-    {
-        if (TIFFWriteScanline(tif, ((char *)buffer) + (bytes * row * width), row, 0) == -1) // TODO: TIFFWriteScanline may return -1 on failure:
-        {
-            TIFFError("WriteGeotiff_DEM","failure in WriteScanline on row %d\n", row);
-        }
-    }
-    
-    GTIFWriteKeys(gtif);
-    GTIFFree(gtif);
-    XTIFFClose(tif);
-    return 0;
+	return WriteGeotiff_real(filename, buffer, width, height, scale, minX, maxY, projection, zone, NS_hemisphere, data_type, true);
 }
 
 uint8 ReadGeotiff_bits(char *filename)
@@ -235,55 +198,8 @@ CSize ReadGeotiff_info_dxy(char *filename, double *minX, double *maxY, double *g
     return image_size;
 }
 
+
 void SetUpTIFFDirectory(TIFF *tif, size_t width, size_t height, double scale, double minX, double maxY, int data_type)
-{
-    double tiepoints[6] = {0};
-    double pixscale[3] = {0};
-    tiepoints[3] = minX;
-    tiepoints[4] = maxY;
-    pixscale[0] = scale;
-    pixscale[1] = scale;
-
-    TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, width);
-    TIFFSetField(tif, TIFFTAG_IMAGELENGTH, height);
-    TIFFSetField(tif, TIFFTAG_COMPRESSION, COMPRESSION_LZW);
-    TIFFSetField(tif, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);
-    TIFFSetField(tif, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
-    TIFFSetField(tif, TIFFTAG_GEOTIEPOINTS, 6,tiepoints);
-    TIFFSetField(tif, TIFFTAG_GEOPIXELSCALE, 3,pixscale);
-    TIFFSetField(tif, TIFFTAG_PREDICTOR, 1);
-    TIFFSetField(tif, TIFFTAG_SAMPLESPERPIXEL, 1);
-    
-    //TIFFSetField(tif, TIFFTAG_TILEWIDTH, 256);
-    //TIFFSetField(tif, TIFFTAG_TILELENGTH,  256);
-    
-    switch (data_type)
-    {
-        case FLOAT:
-            TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 32);
-            TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_IEEEFP);
-            TIFFMergeFieldInfo(tif, xtiffFieldInfo, N(xtiffFieldInfo));
-            TIFFSetField(tif, GDAL_NODATA, "-9999");
-            TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, max(1, (STRIP_SIZE_DEFAULT * 8) / (width * 32)));
-            break;
-        case UCHAR:
-            TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 8);
-            TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_UINT);
-            TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, 2);
-            TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, max(1, (STRIP_SIZE_DEFAULT * 8) / (width * 8)));
-            break;
-        case UINT16:
-            TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 16);
-            TIFFSetField(tif, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_UINT);
-            TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, 1);
-            TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, max(1, (STRIP_SIZE_DEFAULT * 8) / (width * 16)));
-            break;
-        default:
-            break;
-    }
-}
-
-void SetUpTIFFDirectory_round(TIFF *tif, size_t width, size_t height, double scale, double minX, double maxY, int data_type)
 {
     double tiepoints[6] = {0};
     double pixscale[3] = {0};
