setsmdeveloper/SETSM

commit ab746df6a7a0af981f177a83d0291ebc3a2db233

g++ and icpc build without any warnings. icx/icpx was added to Makefile as a compiler, all warnings fixed.
	modified:   Coregistration.cpp
	modified:   Makefile
	modified:   Orthogeneration.cpp
	modified:   SDM.cpp
	modified:   SubFunctions.cpp
	modified:   Template.hpp
	modified:   setsm_code.cpp

setsm_code.cpp
@@ -3843,7 +3843,7 @@ int Matching_SETSM(ProInfo *proinfo,const uint8 pyramid_step, const uint8 Templa
                   
                             const long int Accessable_grid = VerticalLineLocus(grid_voxel,proinfo,nccresult,levelinfo,GridPT3,iteration,minmaxHeight);
                             
-                            printf("Done VerticalLineLocus\tgrid %d\n",Accessable_grid);
+                            printf("Done VerticalLineLocus\tgrid %ld\n",Accessable_grid);
                             
                             if(!check_matching_rate)
                             {
@@ -3876,7 +3876,7 @@ int Matching_SETSM(ProInfo *proinfo,const uint8 pyramid_step, const uint8 Templa
                             
                             count_MPs = SelectMPs(proinfo, levelinfo, nccresult, GridPT3, Th_roh, Th_roh_min, Th_roh_start, Th_roh_next, iteration, MPP, final_level_iteration, MPP_stereo_angle, &MatchedPts_list);
                             
-                            printf("row = %d\tcol = %d\tlevel = %d\titeration = %d\tEnd SelectMPs\tcount_mps = %d\t%d\n",row,col,level,iteration,count_MPs,MatchedPts_list.size());
+                            printf("row = %d\tcol = %d\tlevel = %d\titeration = %d\tEnd SelectMPs\tcount_mps = %d\t%zu\n",row,col,level,iteration,count_MPs,MatchedPts_list.size());
                             
                             D3DPOINT *ptslists = NULL;
                             
@@ -3979,7 +3979,7 @@ int Matching_SETSM(ProInfo *proinfo,const uint8 pyramid_step, const uint8 Templa
                             {
                                 if(check_ortho_cal && proinfo->IsRA != 1)
                                 {
-                                    printf("settingflag %d\t%d\n",MatchedPts_list_anchor.size(),MatchedPts_list_blunder.size());
+                                    printf("settingflag %zu\t%zu\n",MatchedPts_list_anchor.size(),MatchedPts_list_blunder.size());
                                     count_MPs = SetttingFlagOfGrid(levelinfo, GridPT3, MatchedPts_list_anchor, MatchedPts_list_blunder, &MatchedPts_list_mps);
                                     MatchedPts_list_anchor.clear();
                                     vector<D3DPOINT>().swap(MatchedPts_list_anchor);
@@ -3987,7 +3987,7 @@ int Matching_SETSM(ProInfo *proinfo,const uint8 pyramid_step, const uint8 Templa
                                     vector<D3DPOINT>().swap(MatchedPts_list_blunder);
                                 }
                                 
-                                printf("count_MPs %d\t%d\n",count_MPs,MatchedPts_list_mps.size());
+                                printf("count_MPs %d\t%zu\n",count_MPs,MatchedPts_list_mps.size());
                                 count_MPs = MatchedPts_list_mps.size();
                                 
                                 vector<UI3DPOINT> t_trilists;
@@ -4149,7 +4149,7 @@ int Matching_SETSM(ProInfo *proinfo,const uint8 pyramid_step, const uint8 Templa
                                         
                                         matching_rate = count_MPs/(double)(Accessable_grid);
                                         
-                                        printf("matching change rate pre curr %f\t%d\t%d\tmatching rate %f\t%d\n",matching_change_rate,count_MPs,pre_matched_pts,matching_rate,Accessable_grid);
+                                        printf("matching change rate pre curr %f\t%d\t%d\tmatching rate %f\t%ld\n",matching_change_rate,count_MPs,pre_matched_pts,matching_rate,Accessable_grid);
                                         pre_matched_pts = count_MPs;
                                         
                                         if(level <= 2 && matching_rate < th_mr && proinfo->DEM_resolution <= 4)
@@ -4337,7 +4337,7 @@ int Matching_SETSM(ProInfo *proinfo,const uint8 pyramid_step, const uint8 Templa
                                         }
                                         
                                         matching_rate = count_MPs/(double)(Accessable_grid);
-                                        printf("matching change rate pre curr %f\t%d\t%d\tTh_roh %f\t%f\tmatching rate %f\t%d\n",matching_change_rate,count_MPs,pre_matched_pts,Th_roh,Th_roh_min,matching_rate,Accessable_grid);
+                                        printf("matching change rate pre curr %f\t%d\t%d\tTh_roh %f\t%f\tmatching rate %f\t%ld\n",matching_change_rate,count_MPs,pre_matched_pts,Th_roh,Th_roh_min,matching_rate,Accessable_grid);
                                         
                                         if(level <= 2 && matching_rate < th_mr && proinfo->DEM_resolution <= 4)
                                             level_check_matching_rate = true;
@@ -5426,7 +5426,7 @@ void SetHeightWithSeedDEM(const ProInfo *proinfo, LevelInfo &rlevelinfo, UGRID *
 
             float type(0);
             float *seeddem = Readtiff_T(GIMP_path,LImagesize,cols,rows,&data_size, type);
-            printf("Grid size %d\t%d\tcols rows %d\t%d\t%d\t%d\n",rlevelinfo.Size_Grid2D->width,rlevelinfo.Size_Grid2D->height,cols[0],cols[1],rows[0],rows[1]);
+            printf("Grid size %d\t%d\tcols rows %ld\t%ld\t%ld\t%ld\n",rlevelinfo.Size_Grid2D->width,rlevelinfo.Size_Grid2D->height,cols[0],cols[1],rows[0],rows[1]);
 
             SetGridHeightFromSeed(rlevelinfo, Grid, seeddem, seeddem_size, grid_size, minX, maxY, seedDEM_sigma, minmaxHeight);
             
@@ -7054,7 +7054,7 @@ void AWNCC(ProInfo *proinfo, VOXEL **grid_voxel,CSize Size_Grid2D, UGRID *GridPT
             {
                 long t_index = i*(long)Size_Grid2D.width + j;
                 if(Pyramid_step == 0 && iteration == 3 && nccresult[t_index].NumOfHeight > 2000)
-                    printf("gridsize %d\t%d\t pos %d\t%d\t numofheight %d\t%d\t%d\n",Size_Grid2D.width,Size_Grid2D.height,j,i,nccresult[t_index].NumOfHeight,nccresult[t_index].maxHeight,nccresult[t_index].minHeight);
+                    printf("gridsize %d\t%d\t pos %ld\t%ld\t numofheight %d\t%d\t%d\n",Size_Grid2D.width,Size_Grid2D.height,j,i,nccresult[t_index].NumOfHeight,nccresult[t_index].maxHeight,nccresult[t_index].minHeight);
                 if(nccresult[t_index].NumOfHeight > 0)
                 {
                     SumCost[t_index] = (float*)calloc(sizeof(float),nccresult[t_index].NumOfHeight);
@@ -7333,7 +7333,7 @@ void AWNCC(ProInfo *proinfo, VOXEL **grid_voxel,CSize Size_Grid2D, UGRID *GridPT
                 }
              
 #pragma omp for
-                for(ref_iter = start_col[direction_iter] ; ref_iter >= end_col[direction_iter] ; ref_iter = ref_iter + col_iter[direction_iter]) //top wall col direction
+                for(ref_iter = start_col[direction_iter] ; ref_iter < end_col[direction_iter] ; ref_iter = ref_iter + col_iter[direction_iter]) //top wall col direction
                 {
                     pts_col = ref_iter;
                     bool check_free_LHcost_pre = false;
@@ -7357,7 +7357,7 @@ void AWNCC(ProInfo *proinfo, VOXEL **grid_voxel,CSize Size_Grid2D, UGRID *GridPT
              
                             if(pts_col >= 0 && pts_col < Size_Grid2D.width)
                             {
-                                long pt_index = pts_row*Size_Grid2D.width + pts_col;
+                                long pt_index = pts_row*(long)Size_Grid2D.width + pts_col;
              
                                 memset(LHcost_curr, 0, nccresult[pt_index].NumOfHeight*sizeof(float));
                                 SGM_con_pos(pts_col, pts_row, Size_Grid2D, direction_iter, step_height, P_HS_step, u_col, v_row, nccresult, grid_voxel, GridPT3, pt_index, P1, P2, LHcost_pre, LHcost_curr, SumCost);
@@ -7377,7 +7377,7 @@ void AWNCC(ProInfo *proinfo, VOXEL **grid_voxel,CSize Size_Grid2D, UGRID *GridPT
                 //printf("bottom left to right\n");
                 direction_iter = 6;
 #pragma omp for
-                for(ref_iter = start_row[direction_iter] ; ref_iter >= end_row[direction_iter] ; ref_iter = ref_iter + row_iter[direction_iter]) //left wall row direction
+                for(ref_iter = start_row[direction_iter] ; ref_iter < end_row[direction_iter] ; ref_iter = ref_iter + row_iter[direction_iter]) //left wall row direction
                 {
                     pts_row = ref_iter;
                     bool check_free_LHcost_pre = false;
@@ -7459,7 +7459,7 @@ void AWNCC(ProInfo *proinfo, VOXEL **grid_voxel,CSize Size_Grid2D, UGRID *GridPT
                 //printf("bottom right to left\n");
                 direction_iter = 7;
 #pragma omp for
-                for(ref_iter = start_row[direction_iter] ; ref_iter >= end_row[direction_iter] ; ref_iter = ref_iter + row_iter[direction_iter]) //left wall row direction
+                for(ref_iter = start_row[direction_iter] ; ref_iter < end_row[direction_iter] ; ref_iter = ref_iter + row_iter[direction_iter]) //left wall row direction
                 {
                     pts_row = ref_iter;
                     bool check_free_LHcost_pre = false;
@@ -7499,7 +7499,7 @@ void AWNCC(ProInfo *proinfo, VOXEL **grid_voxel,CSize Size_Grid2D, UGRID *GridPT
                 }
              
 #pragma omp for
-                for(ref_iter = start_col[direction_iter] ; ref_iter >= end_col[direction_iter] ; ref_iter = ref_iter + col_iter[direction_iter]) //top wall col direction
+                for(ref_iter = start_col[direction_iter] ; ref_iter < end_col[direction_iter] ; ref_iter = ref_iter + col_iter[direction_iter]) //top wall col direction
                 {
                     pts_col = ref_iter;
                     bool check_free_LHcost_pre = false;
@@ -11237,7 +11237,7 @@ void MergeTiles(const ProInfo *info, const int iter_row_start, const int t_col_s
                                    &t_row,&t_col,&t_level,&t_boundary[0],&t_boundary[1],&t_grid_size,&col_size,&row_size);
                         }
                         
-                        printf("header %f\t%f\t%d\t%d\t%f\t%d\n",t_boundary[0],t_boundary[1],col_size,row_size,grid_size,buffer);
+                        printf("header %f\t%f\t%ld\t%ld\t%f\t%d\n",t_boundary[0],t_boundary[1],col_size,row_size,grid_size,buffer);
                         
                         char hv_t_str[500];
                         sprintf(hv_t_str,"%s/txt/tin_h_level_%d_%d_%d_iter_%d_final.txt",info->save_filepath,row,col,find_level,final_iteration);
@@ -11405,7 +11405,7 @@ void NNA_M(const ProInfo *proinfo, const TransParam _param, const int row_start,
     FILE *fheader = fopen(DEM_header,"r");
     double dummy;
     fscanf(fheader,"%lf\t%lf\t%lf\t%ld\t%ld",&DEM_minX,&DEM_maxY,&dummy,&DEM_cols,&DEM_rows);
-    printf("%f\t%f\t%d\t%d\n",DEM_minX,DEM_maxY,DEM_cols,DEM_rows);
+    printf("%f\t%f\t%ld\t%ld\n",DEM_minX,DEM_maxY,DEM_cols,DEM_rows);
     fclose(fheader);
     
     remove(DEM_header);
@@ -11463,7 +11463,7 @@ void NNA_M(const ProInfo *proinfo, const TransParam _param, const int row_start,
         if(row >= row_start && row <= row_end && col >= col_start &&  col <= col_end)
         {
             char t_str[500];
-            sprintf(t_str,"%s/txt/matched_pts_%d_%d_0_%d.txt",proinfo->save_filepath,row,col,final_iteration);
+            sprintf(t_str,"%s/txt/matched_pts_%ld_%ld_0_%d.txt",proinfo->save_filepath,row,col,final_iteration);
             FILE *pfile   = fopen(t_str,"rb");
             if(pfile)
             {
@@ -11476,10 +11476,10 @@ void NNA_M(const ProInfo *proinfo, const TransParam _param, const int row_start,
                     char h_t_str[500];
                     char c_t_str[500];
                     
-                    sprintf(h_t_str,"%s/txt/headerinfo_row_%d_col_%d.txt",proinfo->save_filepath,row,col);
+                    sprintf(h_t_str,"%s/txt/headerinfo_row_%ld_col_%ld.txt",proinfo->save_filepath,row,col);
                     FILE *p_hfile     = fopen(h_t_str,"r");
                     
-                    sprintf(c_t_str,"%s/txt/count_row_%d_col_%d.txt",proinfo->save_filepath,row,col);
+                    sprintf(c_t_str,"%s/txt/count_row_%ld_col_%ld.txt",proinfo->save_filepath,row,col);
                     FILE *c_hfile     = fopen(c_t_str,"r");
                     
                     if(p_hfile)
@@ -11500,7 +11500,7 @@ void NNA_M(const ProInfo *proinfo, const TransParam _param, const int row_start,
                         fscanf(c_hfile,"%ld\n",&count_MPs);
                         D3DPOINTSAVE *temp_pts = (D3DPOINTSAVE*)malloc(sizeof(D3DPOINTSAVE)*count_MPs);
                         fread(temp_pts,sizeof(D3DPOINTSAVE),count_MPs,pfile);
-                        printf("read count_MPs %d\t%d\n",count_MPs,buffer_clip);
+                        printf("read count_MPs %ld\t%d\n",count_MPs,buffer_clip);
                         
                         long count_read = 0;
                         long count_out = 0;
@@ -11718,7 +11718,7 @@ void MergeTiles_Ortho(const ProInfo *info, const int iter_row_start, const int t
         for(long col = t_col_start ; col <= t_col_end ; col++)
         {
             char t_str[500];
-            sprintf(t_str,"%s/txt/matched_pts_%d_%d_%d_%d.txt",info->save_filepath,row,col,find_level,final_iteration);
+            sprintf(t_str,"%s/txt/matched_pts_%ld_%ld_%d_%d.txt",info->save_filepath,row,col,find_level,final_iteration);
             FILE *pfile   = fopen(t_str,"r");
             if(pfile)
             {
@@ -11728,7 +11728,7 @@ void MergeTiles_Ortho(const ProInfo *info, const int iter_row_start, const int t
                 if(size > 0)
                 {
                     char h_t_str[500];
-                    sprintf(h_t_str,"%s/txt/headerinfo_row_%d_col_%d.txt",info->save_filepath,row,col);
+                    sprintf(h_t_str,"%s/txt/headerinfo_row_%ld_col_%ld.txt",info->save_filepath,row,col);
                     FILE *p_hfile     = fopen(h_t_str,"r");
                     if(p_hfile)
                     {
@@ -11744,7 +11744,7 @@ void MergeTiles_Ortho(const ProInfo *info, const int iter_row_start, const int t
                         }
                         
                         char ortho_str[500];
-                        sprintf(ortho_str,"%s/txt/tin_ortho_ncc_level_%d_%d_%d_iter_%d_final.txt",info->save_filepath,row,col,find_level,final_iteration);
+                        sprintf(ortho_str,"%s/txt/tin_ortho_ncc_level_%ld_%ld_%d_iter_%d_final.txt",info->save_filepath,row,col,find_level,final_iteration);
                         FILE *p_orthofile = fopen(ortho_str,"rb");
                         
                         if(p_orthofile)
@@ -11989,7 +11989,7 @@ void NNA_M_MT(const ProInfo *proinfo, const TransParam _param, const int row_sta
         
         check_size = 0;
         max_total_iteration = 3;
-        total_iteration;
+        total_iteration = 0;
         
         for(total_iteration = 1  ; total_iteration <= max_total_iteration ; total_iteration++)
         {

Coregistration.cpp
@@ -314,7 +314,7 @@ double** ImageCoregistration(TransParam *return_param, char* _filename, ARGINFO 
                     
                     CoregParam_Image(proinfo, ti, level, ImageAdjust_coreg[ti], 15, SubImages_ref, data_size_lr[reference_id][level], SubImages_tar, data_size_lr[ti][level], ImageBoundary[reference_id], ImageBoundary[ti], grid_dxy_ref, grid_dxy_tar, Grid_space[level], Boundary[ti], &avg_roh, &iter_counts, &adjust_std[ti], matched_MPs, matched_MPs_ref, MPs);
                      
-                    printf("%d\t%d\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%d\t%4.2f\t%d\n",level,ti,ImageAdjust_coreg[ti][0], ImageAdjust_coreg[ti][1],
+                    printf("%d\t%d\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%zu\t%4.2f\t%d\n",level,ti,ImageAdjust_coreg[ti][0], ImageAdjust_coreg[ti][1],
                            -ImageAdjust_coreg[ti][0]*ortho_dy[ti], ImageAdjust_coreg[ti][1]*ortho_dx[ti],matched_MPs.size(),avg_roh,iter_counts);
                     free(SubImages_ref);
                     free(SubImages_tar);
@@ -800,7 +800,7 @@ void DEM_ImageCoregistration_hillshade(TransParam *return_param, char* _filename
                     
                     CoregParam_Image(proinfo, ti, level, ImageAdjust_coreg, 15, SubImages_ref[level], data_size_ref[level], SubImages_tar[level],  data_size_tar[level], ImageBoundary_ref, ImageBoundary_tar, grid_dxy_ref, grid_dxy_tar, Grid_space, Boundary, &avg_roh, &iter_counts, &adjust_std, MPs_2D_mat, MPs_2D, MPs);
                     
-                    printf("%d\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%d\t%4.2f\t%d\t%4.2f\t%4.2f\n",level,ImageAdjust_coreg[0], ImageAdjust_coreg[1],-ImageAdjust_coreg[0]*tar_dy, ImageAdjust_coreg[1]*tar_dx,MPs_2D.size(),avg_roh,iter_counts,adjust_std.m_X,adjust_std.m_Y);
+                    printf("%d\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%zu\t%4.2f\t%d\t%4.2f\t%4.2f\n",level,ImageAdjust_coreg[0], ImageAdjust_coreg[1],-ImageAdjust_coreg[0]*tar_dy, ImageAdjust_coreg[1]*tar_dx,MPs_2D.size(),avg_roh,iter_counts,adjust_std.m_X,adjust_std.m_Y);
                     free(SubImages_tar[level]);
                 }
                 free(SubImages_tar);
@@ -1477,7 +1477,7 @@ void DEM_ImageCoregistration_GeomatricConstraint(TransParam *return_param, char*
                 vector<D3DPOINT> select_pts_tar;
                 SettingControls(proinfo, DEM, DEM_tar, ref_dx, tar_dx, ImageBoundary_ref, ImageBoundary_tar, Boundary, ref_dem_size, tar_dem_size, tin_point_num, select_pts_tar);
                 
-                printf("select point %d\t%d\n",tin_point_num, select_pts_tar.size());
+                printf("select point %ld\t%zu\n",tin_point_num, select_pts_tar.size());
                 
                 if(tin_point_num > 0)
                 {
@@ -1514,7 +1514,7 @@ void DEM_ImageCoregistration_GeomatricConstraint(TransParam *return_param, char*
                     max_nor_coord.m_Y = (Boundary[3] - coord_center.m_Y)/coord_scale.m_Y;
                     max_nor_coord.m_Z = (max_H - coord_center.m_Z)/coord_scale.m_Z;
                     
-                    printf("total pts %d\n",tin_point_num);
+                    printf("total pts %ld\n",tin_point_num);
                     
                     vector<double> weight(0.0);
                     vector<double>::iterator w_it;
@@ -1732,7 +1732,7 @@ void DEM_ImageCoregistration_GeomatricConstraint(TransParam *return_param, char*
                         {
                             total_ET_ad = time(0);
                             total_gap = difftime(total_ET_ad,total_ST_ad);
-                            printf("\npre matrix adjustment time %f\t%d\n\n",total_gap,number_of_selected);
+                            printf("\npre matrix adjustment time %f\t%ld\n\n",total_gap,number_of_selected);
                             total_ST_ad = time(0);
                             
                             dX = CoeffMatrix_25D(coord_center,coord_scale, number_of_selected, transformed_coord, SDistance, conparam, tar_normal_save,weight,sigmaX,&sigma0);
@@ -1858,11 +1858,11 @@ void DEM_ImageCoregistration_GeomatricConstraint(TransParam *return_param, char*
                             for(int i=0;i<count_vertical;i++)
                                 sum_var_med += (MED_distance -save_Z_vertical[i])*(MED_distance -save_Z_vertical[i]);
                    
-                            printf("iter %d\tparam %f\t%f\t%f\t%f\t%f\t%f\t%d\t%f\t%f\t%f\n",while_iter,conparam.Tx*coord_scale.m_X,conparam.Ty*coord_scale.m_Y,conparam.Tz*coord_scale.m_Z,sigmaX[4]*sigma0*coord_scale.m_X,sigmaX[5]*sigma0*coord_scale.m_Y,sigmaX[6]*sigma0*coord_scale.m_Z, number_of_selected,sigma0*coord_scale.m_Z,change_ratio_sigma,th_dH*ori_scale_Z);
+                            printf("iter %d\tparam %f\t%f\t%f\t%f\t%f\t%f\t%ld\t%f\t%f\t%f\n",while_iter,conparam.Tx*coord_scale.m_X,conparam.Ty*coord_scale.m_Y,conparam.Tz*coord_scale.m_Z,sigmaX[4]*sigma0*coord_scale.m_X,sigmaX[5]*sigma0*coord_scale.m_Y,sigmaX[6]*sigma0*coord_scale.m_Z, number_of_selected,sigma0*coord_scale.m_Z,change_ratio_sigma,th_dH*ori_scale_Z);
                             while_iter++;
                             
                             SD_z_med = sqrt(sum_var_med/count_vertical);
-                            printf("\nTz average and variation  %d\t%f\t%f\t%f\t%f\t%f\n\n",final_number_of_selected,th_dH*ori_scale_Z,vertical_average_distance,MED_distance,SD_distance_vertical,SD_z_med);
+                            printf("\nTz average and variation  %ld\t%f\t%f\t%f\t%f\t%f\n\n",final_number_of_selected,th_dH*ori_scale_Z,vertical_average_distance,MED_distance,SD_distance_vertical,SD_z_med);
                             save_Z_vertical.clear();
                             total_ET = time(0);
                             total_gap = difftime(total_ET,total_ST);
@@ -2017,7 +2017,7 @@ void DEM_ImageCoregistration_GeomatricConstraint(TransParam *return_param, char*
                         if(sigma0*coord_scale.m_Z < 2 && vertical_average_distance < 1.0 && SD_distance_vertical < 5.0 && final_number_of_selected > 100 && sigmaX[4]*sigma0*coord_scale.m_X*(sqrt(distance_scale/coord_scale.m_X)) < tar_dx/2.0 && sigmaX[5]*sigma0*coord_scale.m_Y*(sqrt(distance_scale/coord_scale.m_Y)) < tar_dx/2.0)
                         {
                             
-                               fprintf(fid_out,"%s\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%d\t\t%4.2f\n",DEM_name_outfile,sigma0*coord_scale.m_Z,conparam.Tx*coord_scale.m_X,conparam.Ty*coord_scale.m_Y,conparam.Tz*coord_scale.m_Z,sigmaX[4]*sigma0*coord_scale.m_X*(sqrt(distance_scale/coord_scale.m_X)),sigmaX[5]*sigma0*coord_scale.m_Y*(sqrt(distance_scale/coord_scale.m_Y)),sigmaX[6]*sigma0*coord_scale.m_Z*(sqrt(distance_scale/coord_scale.m_Z)),vertical_average_distance,SD_distance_vertical, MED_distance,SD_z_med,all_average,all_med,all_std,final_number_of_selected,total_gap_iter);
+                               fprintf(fid_out,"%s\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%ld\t\t%4.2f\n",DEM_name_outfile,sigma0*coord_scale.m_Z,conparam.Tx*coord_scale.m_X,conparam.Ty*coord_scale.m_Y,conparam.Tz*coord_scale.m_Z,sigmaX[4]*sigma0*coord_scale.m_X*(sqrt(distance_scale/coord_scale.m_X)),sigmaX[5]*sigma0*coord_scale.m_Y*(sqrt(distance_scale/coord_scale.m_Y)),sigmaX[6]*sigma0*coord_scale.m_Z*(sqrt(distance_scale/coord_scale.m_Z)),vertical_average_distance,SD_distance_vertical, MED_distance,SD_z_med,all_average,all_med,all_std,final_number_of_selected,total_gap_iter);
                             
                            /* fprintf(fid_out,"%s\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%d\t\t%4.2f\n",DEM_name_outfile,sigma0*distance_scale,conparam.Tx*coord_scale.m_X,conparam.Ty*coord_scale.m_Y,conparam.Tz*coord_scale.m_Z,sigmaX[4]*sigma0*coord_scale.m_X,sigmaX[5]*sigma0*coord_scale.m_Y,sigmaX[6]*sigma0*coord_scale.m_Z,vertical_average_distance,SD_distance_vertical, MED_distance,SD_z_med,all_average,all_med,all_std,final_number_of_selected,total_gap_iter);
                             
@@ -2026,12 +2026,12 @@ void DEM_ImageCoregistration_GeomatricConstraint(TransParam *return_param, char*
                         }
                         else
                         {
-                            fprintf(fid_out,"%s\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%d\t\t%4.2f\n",DEM_name_outfile,sigma0*coord_scale.m_Z,conparam.Tx*coord_scale.m_X,conparam.Ty*coord_scale.m_Y,conparam.Tz*coord_scale.m_Z,sigmaX[4]*sigma0*coord_scale.m_X*(sqrt(distance_scale/coord_scale.m_X)),sigmaX[5]*sigma0*coord_scale.m_Y*(sqrt(distance_scale/coord_scale.m_Y)),sigmaX[6]*sigma0*coord_scale.m_Z*(sqrt(distance_scale/coord_scale.m_Z)),vertical_average_distance,SD_distance_vertical, MED_distance,SD_z_med,all_average,all_med,all_std,final_number_of_selected,total_gap_iter);
+                            fprintf(fid_out,"%s\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%4.2f\t\t%ld\t\t%4.2f\n",DEM_name_outfile,sigma0*coord_scale.m_Z,conparam.Tx*coord_scale.m_X,conparam.Ty*coord_scale.m_Y,conparam.Tz*coord_scale.m_Z,sigmaX[4]*sigma0*coord_scale.m_X*(sqrt(distance_scale/coord_scale.m_X)),sigmaX[5]*sigma0*coord_scale.m_Y*(sqrt(distance_scale/coord_scale.m_Y)),sigmaX[6]*sigma0*coord_scale.m_Z*(sqrt(distance_scale/coord_scale.m_Z)),vertical_average_distance,SD_distance_vertical, MED_distance,SD_z_med,all_average,all_med,all_std,final_number_of_selected,total_gap_iter);
                         }
                     }
                     else
                     {
-                        fprintf(fid_out,"%s\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\t%d\t\tNaN\n",DEM_name_outfile,final_number_of_selected);
+                        fprintf(fid_out,"%s\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\t%ld\t\tNaN\n",DEM_name_outfile,final_number_of_selected);
                     }
                     free(dX);
                     free(sigmaX);
@@ -2042,7 +2042,7 @@ void DEM_ImageCoregistration_GeomatricConstraint(TransParam *return_param, char*
                 }
                 else
                 {
-                    fprintf(fid_out,"%s\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\t%d\t\tNaN\n",DEM_name_outfile,tin_point_num);
+                    fprintf(fid_out,"%s\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\tNaN\t\t%ld\t\tNaN\n",DEM_name_outfile,tin_point_num);
                 }
                 free(DEM_tar);
                 free(data_size_tar);
@@ -2589,7 +2589,7 @@ void SettingControls(const ProInfo *proinfo, const float* DEM_ref, const float* 
         }
     }
 
-    printf("SettingControls %d\n",tin_point_num);
+    printf("SettingControls %ld\n",tin_point_num);
 }
 
 double* CoeffMatrix_25D(const D3DPOINT coord_center, const D3DPOINT coord_scale, const long selected_pts, const vector<D3DPOINT> &transformed_coord, const vector<double> &dH, const Conformalparam param, const vector<D3DPOINT> &tar_normal, const vector<double> &weight, double* sigmaX, double *sigma0)

commit 1718db23a390cbeed6325a6a1ca7d6f74a14efa5

Fix bug introduced in ab746df6a7a0af981f177a83d0291ebc3a2db233

setsm_code.cpp
@@ -25,7 +25,7 @@
 #include "mpi_helpers.hpp"
 #endif
 
-const char setsm_version[] = "4.3.12";
+const char setsm_version[] = "4.3.14";
 
 int main(int argc,char *argv[])
 {
@@ -7297,7 +7297,7 @@ void AWNCC(ProInfo *proinfo, VOXEL **grid_voxel,CSize Size_Grid2D, UGRID *GridPT
                 }
              
 #pragma omp for
-                for(ref_iter = start_col[direction_iter] ; ref_iter < end_col[direction_iter] ; ref_iter = ref_iter + col_iter[direction_iter]) //top wall col direction
+                for(ref_iter = start_col[direction_iter] ; ref_iter >= end_col[direction_iter] ; ref_iter = ref_iter + col_iter[direction_iter]) //top wall col direction
                 {
                     pts_col = ref_iter;
                     // bool check_free_LHcost_pre = false;
@@ -7339,7 +7339,7 @@ void AWNCC(ProInfo *proinfo, VOXEL **grid_voxel,CSize Size_Grid2D, UGRID *GridPT
                 //printf("bottom left to right\n");
                 direction_iter = 6;
 #pragma omp for
-                for(ref_iter = start_row[direction_iter] ; ref_iter < end_row[direction_iter] ; ref_iter = ref_iter + row_iter[direction_iter]) //left wall row direction
+                for(ref_iter = start_row[direction_iter] ; ref_iter >= end_row[direction_iter] ; ref_iter = ref_iter + row_iter[direction_iter]) //left wall row direction
                 {
                     pts_row = ref_iter;
                     // bool check_free_LHcost_pre = false;
@@ -7421,7 +7421,7 @@ void AWNCC(ProInfo *proinfo, VOXEL **grid_voxel,CSize Size_Grid2D, UGRID *GridPT
                 //printf("bottom right to left\n");
                 direction_iter = 7;
 #pragma omp for
-                for(ref_iter = start_row[direction_iter] ; ref_iter < end_row[direction_iter] ; ref_iter = ref_iter + row_iter[direction_iter]) //left wall row direction
+                for(ref_iter = start_row[direction_iter] ; ref_iter >= end_row[direction_iter] ; ref_iter = ref_iter + row_iter[direction_iter]) //left wall row direction
                 {
                     pts_row = ref_iter;
                     // bool check_free_LHcost_pre = false;
@@ -7461,7 +7461,7 @@ void AWNCC(ProInfo *proinfo, VOXEL **grid_voxel,CSize Size_Grid2D, UGRID *GridPT
                 }
              
 #pragma omp for
-                for(ref_iter = start_col[direction_iter] ; ref_iter < end_col[direction_iter] ; ref_iter = ref_iter + col_iter[direction_iter]) //top wall col direction
+                for(ref_iter = start_col[direction_iter] ; ref_iter >= end_col[direction_iter] ; ref_iter = ref_iter + col_iter[direction_iter]) //top wall col direction
                 {
                     pts_col = ref_iter;
                     // bool check_free_LHcost_pre = false;